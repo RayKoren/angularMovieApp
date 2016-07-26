2184e57989ddf56c4a1d5524cfc609d35a5ea169
rayk216 <raykoren@gmail.com>
2016-07-26 12:17:37 -0600

    unit2

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
-var app = angular.module("movieApp", []);
+$( document ).ready(function(){
+  $(".button-collapse").sideNav();
+
+});
+
+
+// var app = angular.module("movieApp", []);
+angular.module("applydigest", []).controller("MainController", function($rootScope, $scope) {
+  $rootScope.rootView = {};
+  $rootScope.rootView.name = "Fido";
+  $scope.view = {};
+  $scope.view.age = 3;
+
+  // this is for example purposes
+  // NOTE - there is a $timeout which handles $apply for you
+  setTimeout(function() {
+    $rootScope.rootView.name = "Lassie";
+    $scope.view.age = 10;
+    // $scope.$digest();
+    $scope.$apply();
+  }, 1000);
+});
